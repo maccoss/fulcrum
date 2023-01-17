8da32291b745ef59727e1da6bc955ba9482dc506
Seth Just <sethjust@sethjust.com>
2023-01-17 14:15:34 -0800

    Initial config for Scry

---
+++ scry/__init__.py
@@ -0,0 +1,27 @@
+"""
+scry: Extreme-scale proteomics pipeline
+
+Exports:
+
+- TODO
+"""
+
+# Initialize the package.
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version("scry-ms")
+    except PackageNotFoundError:
+        pass
+
+except ImportError:
+    from pkg_resources import get_distribution, DistributionNotFound
+
+    try:
+        __version__ = get_distribution("scry-ms").version
+    except DistributionNotFound:
+        pass
+
+# Here is where we can export public functions and classes.
+#from .package import Symbol  # import relative to this package to avoid namespace collisions
