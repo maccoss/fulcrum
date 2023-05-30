f633e54771117c8904df72681eafef84e0ed2d91
Seth Just <sjust@seer.bio>
2023-05-30 15:24:00 -0700

    Outline TODOs. Fix lint.

--- scry/output/library.py
+++ scry/output/library.py
@@ -2,14 +2,26 @@
 `scry.output.library` -- workflow module that supports transforming sets of PSMs into a format
 suitable for use with library search tools.
 """
+from wheely.mammoth import (
+    PsmDataset as _PsmDataset,
+    ConfidenceDataset as _ConfidenceDataset,
+)
 
 
-def write_library(data: _ConfidenceDataset, location: str, **kwargs):
+def write_library(data: _PsmDataset, location: str, **kwargs):
     """
-    TODO
+    Write the given dataset to the given location.
+
+    This implementation is still in progress:
+    * TODO: define the format in which the library will be written
+    * TODO: define the source of spectral / RT information
+    * TODO: define any filtering applied to the output library
+
+    Future directions:
+    * TODO: add support for other dimensions: e.g. IM
 
     Returns
     -------
-
+    TODO: document return
     """
     raise NotImplementedError("TODO")
