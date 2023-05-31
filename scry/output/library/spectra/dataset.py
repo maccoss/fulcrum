5f83f582e16ee2bbe88de5e86d1b6b97fc919fc5
Seth Just <sjust@seer.bio>
2023-05-31 14:28:52 -0700

    Stub interface module

---
+++ scry/output/library/spectra/dataset.py
@@ -0,0 +1,11 @@
+"""
+`scry.output.library.spectra` -- interface for  spectral information specifically for use in a library
+"""
+
+
+class SpectraDataset:
+    """
+    TODO
+    """
+
+    raise NotImplementedError("TODO")
