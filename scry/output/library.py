0a61b2af6c26f0ef7e8a841842de8a372d69b59c
Seth Just <sjust@seer.bio>
2023-05-31 09:15:54 -0700

    Begin fleshing out interface, docs for library output function

--- scry/output/library.py
+++ scry/output/library.py
@@ -2,26 +2,57 @@
 `scry.output.library` -- workflow module that supports transforming sets of PSMs into a format
 suitable for use with library search tools.
 """
+from pyspark.sql import Column as _Column
+
 from wheely.mammoth import (
     PsmDataset as _PsmDataset,
     ConfidenceDataset as _ConfidenceDataset,
 )
 
 
-def write_library(data: _PsmDataset, location: str, **kwargs):
+def write_library(
+    data: _PsmDataset,
+    location: str,
+    threshold_col: str | _Column = None,
+    qval_thresh: float = 0.01,
+):
     """
-    Write the given dataset to the given location.
+    Write the given dataset to the given location, formatted for use as a spectral library.
+
+    Filtering -- If the optional `threshold_col` parameter is provided, only rows where this column
+    is `True` will be included in the output. If `theshold_col` is not specified but the datasest is
+    a `wheely.mammoth.ConfidenceDataset` the optional `qval_thresh` parameter will be used to filter
+    PSMs. Otherwise all PSMs in the dataset will be included in the output.
 
     This implementation is still in progress:
     * TODO: define the format in which the library will be written
     * TODO: define the source of spectral / RT information
-    * TODO: define any filtering applied to the output library
 
     Future directions:
     * TODO: add support for other dimensions: e.g. IM
 
+    Parameters
+    ----------
+    data: The dataset
+    location: The output location (path or URI)
+    threshold_col (str | pyspark.sql.Column; optional): A column (or its name) specifying which
+        rows will be included in the resulting library.
+    qval_thresh (float; optional): The largest _q_-value accepted into the library. Ignored if the
+        dataset is not a `wheely.mammoth.ConfidenceDataset` or `threshold_col` is specified.
+
     Returns
     -------
-    TODO: document return
+    `None` on success
     """
-    raise NotImplementedError("TODO")
+
+    # 1. Filter
+    raise NotImplementedError("TODO")  # TODO
+
+    # 2. Join spectral info
+    # TODO
+
+    # 3. Write output
+    # TODO
+
+    # 4. Return
+    return None
