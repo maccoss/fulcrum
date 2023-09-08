32bd82f4a33f533062d898e1874e7fb372980416
Seth Just <sjust@seer.bio>
2023-09-08 10:10:55 -0700

    Add very-simple assertion that write_library correctly applies peptide normalization

--- tests/unit_tests/output/library/test_write.py
+++ tests/unit_tests/output/library/test_write.py
@@ -4,7 +4,8 @@ import pytest
 
 from numpy.testing import assert_array_equal
 
-from pyspark.sql.functions import array, col, lit, rand as _rand
+import pyspark.sql.functions as fns
+from pyspark.sql.functions import array, lit, rand as _rand
 
 from wheely.mammoth import ConfidenceDataset, PsmDataset
 from wheely.mammoth.parsers import read_encyclopedia_features
@@ -72,7 +73,7 @@ def spectra_backend() -> Callable:
             )
             .withColumn("__z", (rand(seed=0) * 4).astype("int"))
             .withColumn("__mz", rand(seed=1) * 600 + 400)
-            .withColumn("__rt", col("__mz") + rand(seed=2) * 200)
+            .withColumn("__rt", fns.col("__mz") + rand(seed=2) * 200)
             .withColumn(
                 "__peaklist",
                 array(
@@ -93,7 +94,7 @@ def spectra_backend() -> Callable:
 @pytest.mark.parametrize(
     "dataset_fixture", ["psm_dataset", "confidence_dataset"]
 )
-def test_create_library(request, dataset_fixture, spectra_backend):
+def test_write_library(request, dataset_fixture, spectra_backend):
     """
     Test that we can build a library DataFrame from our PSM and spectrum fixtures.
     """
@@ -122,6 +123,9 @@ def test_create_library(request, dataset_fixture, spectra_backend):
 
     for col in result.columns:
         assert not col.startswith("__"), f"Leaked internal column? {col}"
+
+    # Assume default sequence / modification normalization
+    assert result.filter(fns.col("ModifiedPeptide").rlike(r"\[")).isEmpty()
 
 
 def test_filter_psms_with_confidence_dataset(confidence_dataset):
@@ -155,7 +159,7 @@ def test_filter_psms_with_psm_dataset(psm_dataset):
 
     # Perform assertions (e.g., check if the filtered dataset contains the expected PSMs)
     assert psm_dataset.data.count() > filtered_dataset.data.count()
-    assert filtered_dataset.data.filter(~col(threshold_col)).isEmpty()
+    assert filtered_dataset.data.filter(~fns.col(threshold_col)).isEmpty()
 
 
 @pytest.mark.parametrize(
