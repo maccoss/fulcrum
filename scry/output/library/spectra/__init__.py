52583ae937570f8c3be655f0388a400a69b25205
Seth Just <sjust@seer.bio>
2023-09-29 10:47:33 -0700

    ⬆️  wheely-mammoth-v0.8.0

--- scry/output/library/spectra/__init__.py
+++ scry/output/library/spectra/__init__.py
@@ -2,5 +2,3 @@
 `scry.output.library.spectra` -- interfaces and implementations for fetching spectral information
 specifically for use in a library
 """
-
-from .registry import get_backend, register_backend
