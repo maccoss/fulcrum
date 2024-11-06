d8d9814270fb6ea8ddc13b8daf852dc2bc603010
Seth Just <sjust@seer.bio>
2024-11-06 09:23:34 -0800

    ✨ Add `write_parquet` backend for output

--- scry/output/basic.py
+++ scry/output/basic.py
@@ -64,3 +64,52 @@ def write_csv(
         header=_kwargs.pop("header", True),
         **_kwargs,
     )
+
+
+def write_parquet(
+    data: _PsmDataset,
+    location: str,
+    threshold_col: _Optional[_Union[str, _Column]] = None,
+    qval_thresh: float = None,
+    include_decoys: bool = False,
+    **kwargs,
+):
+    """
+    Write the given dataset to Parquet. A thin wrapper around `pyspark.sql.DataFrameWrite.parquet`.
+
+    Filtering -- If the optional `threshold_col` parameter is provided, only rows where this column
+    is `True` will be included in the output. If `threshold_col` is not specified but the dataset is
+    a `wheely.mammoth.ConfidenceDataset` the optional `qval_thresh` parameter will be used to filter
+    PSMs. Otherwise all target PSMs in the dataset will be included in the output. To include decoys
+    in the output, pass `include_decoys=True` (note: `include_decoys` is ignored if `threshold_col`
+    is specified).
+
+    Parameters
+    ----------
+    dataset: ConfidenceDataset
+        The dataset to write
+    location: A string specifying where the results should be written
+    threshold_col (str | pyspark.sql.Column; optional): A column (or its name) specifying which
+        rows will be included in the resulting library.
+    qval_thresh (float): The largest _q_-value accepted into the library. Ignored if
+        the dataset is not a `wheely.mammoth.ConfidenceDataset` or `threshold_col` is specified.
+    include_decoys (bool; default = False): If true, include decoy PSMs in the library. Ignored
+        if `threshold_col` is specified.
+    **kwargs: additional keyword arguments to pass to `pyspark.sql.DataFrameWrite.parquet`.
+              Defaults: `{"mode": "errorifexists"}`
+    """
+
+    data_filt = filter_psms(
+        data,
+        threshold_col=threshold_col,
+        qval_thresh=qval_thresh,
+        include_decoys=include_decoys,
+    )
+
+    _kwargs = kwargs.copy()
+
+    data_filt.data.write.parquet(
+        path=location,
+        mode=_kwargs.pop("mode", "errorifexists"),
+        **_kwargs,
+    )
