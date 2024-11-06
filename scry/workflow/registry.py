ad86a05252c9e024f787225b07d406e574dec03f
Seth Just <sjust@seer.bio>
2024-11-06 13:10:38 -0800

    ✨ Make workflows pluggable with a registry

---
+++ scry/workflow/registry.py
@@ -0,0 +1,71 @@
+"""
+`scry.workflow.registry`: allows registering pluggable workflows
+"""
+
+import logging as _logging
+
+# Once the min supported version reaches 3.10, the standard library should
+# be used like so -> from importlib.metadata import entry_points
+from importlib_metadata import entry_points
+
+from .v0 import scry_v0
+
+_logger = _logging.getLogger(__name__)
+
+_workflows = {
+    "v0": scry_v0,
+}
+_plugins = None
+
+
+def register_workflow(name, workflow, clobber=False):
+    assert callable(workflow)
+
+    if name in _workflows:
+        if not clobber:
+            raise RuntimeError(
+                f"Workflow {name} is already registered and `clobber` is False"
+            )
+
+        _logger.warning(
+            f"Replacing already-registered workflow {name} with {workflow}"
+        )
+
+    _workflows[name] = workflow
+
+
+def _get_plugins():
+    """Return a dict of all installed Plugins as {name: EntryPoint}."""
+
+    plugins = entry_points(group="scry.workflow.plugins")
+
+    pluginmap = {}
+    for plugin in plugins:
+        pluginmap[plugin.name] = plugin
+
+    for k, v in pluginmap.items():
+        _logger.debug(f"loading {k}")
+        pluginmap[k] = v.load()
+
+    return pluginmap
+
+
+def get_workflow(name):
+    """Fetch a workflow with the given name."""
+    global _workflows, _plugins
+    try:
+        return _workflows[name]
+    except KeyError as e:
+        if _plugins is None:
+            _plugins = _get_plugins()
+
+        if _plugins is not None and name in _plugins:
+            return _plugins[name]
+
+        all_keys = set(_workflows.keys())
+        if _plugins is not None:
+            all_keys = all_keys.union(_plugins.keys())
+
+        raise KeyError(
+            f"No such workflow {name}. Only {str(all_keys)} are supported"
+        ) from e
