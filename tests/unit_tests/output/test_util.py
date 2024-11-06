d75ddd2e8b5fca767111e9d71290169f3b8f36ce
Seth Just <sjust@seer.bio>
2024-11-06 09:32:41 -0800

    🧪 Add test for corner case in filtering

--- tests/unit_tests/output/test_util.py
+++ tests/unit_tests/output/test_util.py
@@ -60,6 +60,23 @@ def test_filter_psms_with_psm_dataset(psm_dataset):
     assert filtered_dataset.data.filter(~fns.col(threshold_col)).isEmpty()
 
 
+def test_filter_decoys_only(psm_dataset):
+    """
+    Test that we can filter a non-confidence dataset based on decoy/target only
+    """
+    threshold_col = None
+    qval_thresh = None
+    include_decoys = False
+
+    filtered_dataset = filter_psms(
+        psm_dataset, threshold_col, qval_thresh, include_decoys
+    )
+
+    # Perform assertions (e.g., check if the filtered dataset contains the expected PSMs)
+    assert psm_dataset.data.count() >= filtered_dataset.data.count()
+    assert filtered_dataset.data.filter(~filtered_dataset.targets).isEmpty()
+
+
 @pytest.mark.parametrize(
     "dataset_fixture", ["psm_dataset", "confidence_dataset", "spectra_dataset"]
 )
