08c99200ee04680d4ae6008bf5a54b34675c32c4
Seth Just <97198012+sjust-seerbio@users.noreply.github.com>
2025-03-17 14:27:49 -0700

    Update mbr.py

--- scry/workflow/mbr.py
+++ scry/workflow/mbr.py
@@ -258,9 +258,12 @@ def mbr_workflow(
     _logger.info("Wrote library in %.02f sec", lib_write_end - lib_write_start)
 
     search = _copy.deepcopy(search or dict())
+    
+    # remove this parameter if it exists; we'll use the library from the first pass
     search.pop(
         "library", None
-    )  # remove this parameter if it exists; we'll use the library from the first pass
+    )
+    
     if search.pop("use_library_location", False):
         lib = lib_output.get("location", None)
         _logger.info("Using library location %s for search", lib)
